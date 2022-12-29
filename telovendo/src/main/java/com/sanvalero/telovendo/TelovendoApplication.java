ca3e1db9103e47322c74848f6229bcb6010c0c78
Sandra-SFR <sandrafantonirubio@gmail.com>
2022-12-29 15:10:47 +0100

    Initial commit

---
+++ telovendo/src/main/java/com/sanvalero/telovendo/TelovendoApplication.java
@@ -0,0 +1,13 @@
+package com.sanvalero.telovendo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelovendoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelovendoApplication.class, args);
+	}
+
+}
